6f5d9a34ce30a790aad5de77d6dedbdd1bd64b35
Akron254 <aaronrono42@gmail.com>
2020-06-27 11:19:13 +0300

    Interface Implement

--- app/src/main/java/com/kenova/mpesa/mpesa/services/STKPushService.java
+++ app/src/main/java/com/kenova/mpesa/mpesa/services/STKPushService.java
@@ -1,8 +1,24 @@
 package com.kenova.mpesa.mpesa.services;
 
+import com.kenova.mpesa.mpesa.model.AccessToken;
+import com.kenova.mpesa.mpesa.model.STKPush;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
 /**
  * Created by Aaron at 6/27/2020
  **/
 public interface STKPushService
     {
+    @POST("mpesa/stkpush/v1/processrequest")
+    Call<STKPush> sendPush(@Body STKPush stkPush);
+
+    @GET("jobs/pending")
+    Call<STKPush> getTasks();
+
+    @GET("oauth/v1/generate?grant_type=client_credentials")
+    Call<AccessToken> getAccessToken();
     }
