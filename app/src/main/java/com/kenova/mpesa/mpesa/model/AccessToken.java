cdc7cea9aff382894b6539a79dde16ce198eeb6d
Akron254 <aaronrono42@gmail.com>
2020-06-27 11:20:50 +0300

    Created Models

--- app/src/main/java/com/kenova/mpesa/mpesa/model/AccessToken.java
+++ app/src/main/java/com/kenova/mpesa/mpesa/model/AccessToken.java
@@ -1,8 +1,22 @@
 package com.kenova.mpesa.mpesa.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Aaron at 6/27/2020
  **/
 public class AccessToken
     {
+    @SerializedName("access_token")
+    @Expose
+    public String accessToken;
+    @SerializedName("expires_in")
+    @Expose
+    private String expiresIn;
+
+    public AccessToken(String accessToken, String expiresIn) {
+    this.accessToken = accessToken;
+    this.expiresIn = expiresIn;
+    }
     }
